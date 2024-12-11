bb552eb2b52c4e2c482e77bcc5ed21c84fdd8685
Todd <tgnielse1145@gmail.com>
2024-12-11 13:42:02 -0700

    update .gitignore

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /Users/toddnielsen/ECE_6785/2024_Lab3_Sameeran_Todd
+# Build directory: /Users/toddnielsen/ECE_6785/2024_Lab3_Sameeran_Todd/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("FREERTOS_KERNEL")
+subdirs("pico-sdk")
+subdirs("src")
+subdirs("test")
